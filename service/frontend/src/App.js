9f844738e3e545becc63621c7887b1ab75d3472f
Vladarbinyan <clab@mail.ru>
2021-03-24 18:00:38 +0700

    add front authentication

--- service/frontend/src/App.js
+++ service/frontend/src/App.js
@@ -5,8 +5,10 @@ import MainMenu from "./components/Menu";
 import PageNotFound from "./components/PageNotFound";
 import Project from "./components/Project";
 import Todo from "./components/Todo";
+import LoginForm from "./components/Auth";
 import axios from "axios";
-import {BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom'
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
+import Cookies from 'universal-cookie';
 
 
 const DOMAIN = 'http://127.0.0.1:8000'
@@ -19,11 +21,39 @@ class App extends React.Component {
             'users': [],
             'projects': [],
             'todo': [],
+            'token': '',
         }
     }
 
-    async componentDidMount() {
-        await axios.get(get_url('/api/users/'))
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token})
+    }
+
+    is_authenticated() {
+        return this.state.token != ''
+    }
+
+    logout() {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token})
+    }
+
+    get_token(username, password) {
+        axios.post(get_url('/api-token-auth/'), {username: username, password: password})
+            .then(response => {
+                this.set_token(response.data['token'])
+            }).catch(error => alert('Bad login or password!'))
+    }
+
+    load_data() {
+        axios.get(get_url('/api/users/'))
             .then(response => {
                     console.log(response.data)
                     const users = response.data
@@ -34,7 +64,8 @@ class App extends React.Component {
                     )
                 }
             ).catch(error => console.log(error))
-        await axios.get(get_url('/api/projects/'))
+
+        axios.get(get_url('/api/projects/'))
             .then(response => {
                 const projects = response.data
                 this.setState(
@@ -44,7 +75,7 @@ class App extends React.Component {
                 )
             }).catch(error => console.log(error))
 
-        await axios.get(get_url('/api/todo/'))
+        axios.get(get_url('/api/todo/'))
             .then(response => {
                 const todo = response.data
                 this.setState(
@@ -53,12 +84,27 @@ class App extends React.Component {
                     }
                 )
             }).catch(error => console.log(error))
+
+    }
+
+
+    async componentDidMount() {
+        this.get_token_from_storage()
+        this.load_data()
     }
 
     render() {
         return (
             <div>
                 <BrowserRouter>
+                    <nav>
+                        <ul>
+                            <li>
+                                {this.is_authenticated() ? <button onClick={() => this.logout()}>Logout</button> :
+                                    <Link to='/login'>Login</Link>}
+                            </li>
+                        </ul>
+                    </nav>
                     <div>
                         <MainMenu selected_key={"1"}/>
                     </div>
@@ -67,6 +113,8 @@ class App extends React.Component {
                         <Route exact path='/users' component={() => <User users={this.state.users}/>}/>
                         <Route exact path='/projects' component={() => <Project projects={this.state.projects}/>}/>
                         <Route exact path='/todo' component={() => <Todo todo={this.state.todo}/>}/>
+                        <Route exact path='/login' component={() => <LoginForm
+                            get_token={(username, password) => this.get_token(username, password)}/>}/>
                         <Route component={PageNotFound}/>
                     </Switch>
                     <div>
@@ -74,10 +122,6 @@ class App extends React.Component {
                     </div>
                 </BrowserRouter>
             </div>
-
-            // <div>
-
-            // </div>
         )
     }
 }
