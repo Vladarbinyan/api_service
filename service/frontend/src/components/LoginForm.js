b42067fc79292317f71b3e0f87cb91de9fc93763
Vladarbinyan <clab@mail.ru>
2021-04-22 23:39:42 +0700

    update Login form to antd style

--- service/frontend/src/components/LoginForm.js
+++ service/frontend/src/components/LoginForm.js
@@ -1,3 +1,4 @@
+import {Form, Input, Button} from 'antd';
 import React from 'react'
 
 
@@ -15,21 +16,50 @@ class LoginForm extends React.Component {
         );
     }
 
-    handleSubmit(event) {
-        this.props.get_token(this.state.login, this.state.password)
-        event.preventDefault()
+
+    handleSubmit = (values) => {
+        // console.log(values.login, values.password);
+        this.props.get_token(values.login, values.password)
+
     }
+
 
     render() {
         return (
-            <form onSubmit={(event) => this.handleSubmit(event)}>
-                <input type="text" name="login" placeholder="login" value={this.state.login}
-                       onChange={(event) => this.handleChange(event)}/>
-                <input type="password" name="password" placeholder="password" value={this.state.password}
-                       onChange={(event) => this.handleChange(event)}/>
-                <input type="submit" value="Login"/>
-            </form>
-        );
+            <Form onFinish={this.handleSubmit}>
+
+                <Input.Group>
+                    <Form.Item style={{float: 'left'}}
+                               label="Username"
+                               name="login"
+                               rules={[
+                                   {
+                                       required: true,
+                                       message: 'Please input your username!',
+                                   },
+                               ]} value={this.state.login} onChange={(event) => this.handleChange(event)}>
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item style={{float: 'left'}}
+                               label="Password"
+                               name="password"
+                               rules={[
+                                   {
+                                       required: true,
+                                       message: 'Please input your password!',
+                                   },
+                               ]} value={this.state.password} onChange={(event) => this.handleChange(event)}>
+                        <Input.Password/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" value="Login">
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Input.Group>
+            </Form>
+        )
+            ;
     }
 }
 
