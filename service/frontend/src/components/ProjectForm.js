b27ea011ef427edae9e5f4ede0ed9c2bfe1eda3a
Vladarbinyan <clab@mail.ru>
2021-04-23 11:40:36 +0700

    update Login form to antd style

---
+++ service/frontend/src/components/ProjectForm.js
@@ -0,0 +1,62 @@
+// import {Form, Input, Button} from 'antd';
+// import React from 'react'
+//
+//
+// class ProjectForm extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {title: '', users: []}
+//     }
+//
+//     handleChange(event) {
+//         this.setState(
+//             {
+//                 [event.target.name]: event.target.value
+//             }
+//         );
+//     }
+//
+//     handleSubmit = (values) => {
+//         // console.log(values.login, values.password);
+//         this.props.get_token(values.login, values.password)
+//     }
+//
+//     render() {
+//         return (
+//             <Form onFinish={this.handleSubmit}>
+//
+//                 <Input.Group>
+//                     <Form.Item style={{float: 'left'}}
+//                                label="Username"
+//                                name="login"
+//                                rules={[
+//                                    {
+//                                        required: true,
+//                                        message: 'Please input your username!',
+//                                    },
+//                                ]} value={this.state.login} onChange={(event) => this.handleChange(event)}>
+//                         <Input/>
+//                     </Form.Item>
+//                     <Form.Item style={{float: 'left'}}
+//                                label="Password"
+//                                name="password"
+//                                rules={[
+//                                    {
+//                                        required: true,
+//                                        message: 'Please input your password!',
+//                                    },
+//                                ]} value={this.state.password} onChange={(event) => this.handleChange(event)}>
+//                         <Input.Password/>
+//                     </Form.Item>
+//                     <Form.Item>
+//                         <Button type="primary" htmlType="submit" value="Login">
+//                             Login
+//                         </Button>
+//                     </Form.Item>
+//                 </Input.Group>
+//             </Form>
+//         );
+//     }
+// }
+//
+// export default ProjectForm
--- service/frontend/src/components/TodoForm.js
+++ service/frontend/src/components/TodoForm.js
@@ -1,11 +1,11 @@
-import {Form, Input, Button} from 'antd';
+import {Form, Input, Button, Select} from 'antd';
 import React from 'react'
 
 
-class LoginForm extends React.Component {
+class TodoForm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {login: '', password: ''}
+        this.state = {todo: '', text: '', user: '', project: ''}
     }
 
     handleChange(event) {
@@ -17,8 +17,8 @@ class LoginForm extends React.Component {
     }
 
     handleSubmit = (values) => {
-        // console.log(values.login, values.password);
-        this.props.get_token(values.login, values.password)
+        console.log(values);
+        // this.props.get_token(values.login, values.password)
     }
 
     render() {
@@ -27,30 +27,28 @@ class LoginForm extends React.Component {
 
                 <Input.Group>
                     <Form.Item style={{float: 'left'}}
-                               label="Username"
-                               name="login"
+                               label="TODO"
+                               name="todo"
                                rules={[
                                    {
                                        required: true,
-                                       message: 'Please input your username!',
+                                       message: 'Please input TODO title',
                                    },
                                ]}>
                         <Input/>
                     </Form.Item>
+                    <Form.Item style={{float: 'left'}} label="Project" name="project">
+                        <Select >{this.props.projects.map((project)=><option value={project.uuid}>{project.uuid}</option>)}</Select>
+                    </Form.Item>
                     <Form.Item style={{float: 'left'}}
-                               label="Password"
-                               name="password"
-                               rules={[
-                                   {
-                                       required: true,
-                                       message: 'Please input your password!',
-                                   },
-                               ]}>
+                               label="Text"
+                               name="text"
+                    >
                         <Input.Password/>
                     </Form.Item>
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" value="Login">
-                            Login
+                        <Button type="primary" htmlType="submit">
+                            Post data
                         </Button>
                     </Form.Item>
                 </Input.Group>
@@ -59,4 +57,4 @@ class LoginForm extends React.Component {
     }
 }
 
-export default LoginForm
+export default TodoForm
