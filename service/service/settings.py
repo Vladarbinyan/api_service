fa6dba94f9f827e5532af86c295219f9fdc5067e
Vladarbinyan <clab@mail.ru>
2021-04-28 00:06:56 +0700

    add docker dev build

--- service/service/settings.py
+++ service/service/settings.py
@@ -101,10 +101,21 @@ WSGI_APPLICATION = 'service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'pushkin',
+        'PASSWORD': 'dantes1837',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
@@ -170,7 +181,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
-
 
 GRAPHENE = {
     "SCHEMA": "service.schema.schema"
